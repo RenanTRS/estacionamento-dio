b9479f1a07b2b9645211140613213260ab3252ca
Renan Souza <renantrsouza@gmail.com>
2022-05-14 13:00:16 -0400

    Create function for get a date

---
+++ getDate.js
@@ -0,0 +1,10 @@
+export const getDate = () => {
+    const newDate = new Date();
+    const day = String(newDate.getDate()).padStart(2, '0');
+    const month = String(newDate.getMonth() + 1).padStart(2, '0');
+    const year = String(newDate.getFullYear());
+    const hour = String(newDate.getHours()).padStart(2, '0');
+    const minute = String(newDate.getMinutes()).padStart(2, '0');
+    const date = { day, month, year, hour, minute, newDate };
+    return date;
+};
