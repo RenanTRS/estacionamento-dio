9e8db478aab33ce7fdd354a059c8229b809fd776
Renan Souza <renantrsouza@gmail.com>
2022-05-14 19:03:42 -0400

    import function for check the plates and clear inputs

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+import { useVehicle } from './useVehicle.js';
+import { checkPlate } from './checkPlate.js';
+import { getDate } from './getDate.js';
+import { clearInputs } from './clearInputs.js';
+
+(() => {
+    const getInputElement = (query) => document.querySelector(query); //Função para a captura de inputs
+    useVehicle().render(); //Renderiza o storage
+    const inputCarPlate = getInputElement('[data-plate]');
+    inputCarPlate === null || inputCarPlate === void 0 ? void 0 : inputCarPlate.addEventListener('blur', (event) => {
+        const input = event.target;
+        checkPlate(input); //Checa se a placa já existe
+    });
+    const form = document.querySelector('form'); //Formulário
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let name = getInputElement('[data-name]');
+        let plate = getInputElement('[data-plate]');
+        let color = getInputElement('[data-color]');
+        const date = getDate(); //Pega os valores para data
+        const data = {
+            name: name === null || name === void 0 ? void 0 : name.value,
+            color: color === null || color === void 0 ? void 0 : color.value,
+            plate: plate === null || plate === void 0 ? void 0 : plate.value.replace(/[-]/g, '').toUpperCase(),
+            date
+        };
+        useVehicle().add(data, true); //Adiciona veículo
+        clearInputs({ name, plate, color });
+    });
+})();
---
+++ scripts/script.ts
@@ -0,0 +1,36 @@
+import { useVehicle } from './useVehicle.js'
+import { checkPlate } from './checkPlate.js'
+import { getDate } from './getDate.js'
+import { clearInputs } from './clearInputs.js'
+(() => {
+    const getInputElement = (query: string) => document.querySelector(query) as HTMLInputElement | null //Função para a captura de inputs
+    useVehicle().render() //Renderiza o storage
+    
+    const inputCarPlate = getInputElement('[data-plate]')
+    inputCarPlate?.addEventListener('blur', (event) => {
+        const input = event.target as HTMLInputElement
+        checkPlate(input) //Checa se a placa já existe
+    })
+
+    const form = document.querySelector('form') as HTMLFormElement //Formulário
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        let name = getInputElement('[data-name]')
+        let plate = getInputElement('[data-plate]')
+        let color = getInputElement('[data-color]')
+        
+        const date = getDate() //Pega os valores para data
+        
+        const data = {
+            name : name?.value,
+            color: color?.value, 
+            plate: plate?.value.replace(/[-]/g, '').toUpperCase(), //Remove o caractere "-" e transforma em maiúsculo
+            date
+        }
+
+        useVehicle().add(data, true) //Adiciona veículo
+    
+        clearInputs({name, plate, color})
+    })
+})()
